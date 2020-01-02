4ddabcf510ecad42ec7d4e14118063f7db4f87a5
MuEleven <dukaibin521@outlook.com>
2020-01-02 09:20:51 +0800

    第一次

--- app/src/main/java/com/bw/rikao/MainActivity.java
+++ app/src/main/java/com/bw/rikao/MainActivity.java
@@ -1,36 +1,28 @@
 package com.bw.rikao;
 
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv1;
-    private WebView wb1;
+    @BindView(R.id.image)
+    ImageView image;
+    @BindView(R.id.image_xq)
+    ImageView imageXq;
+    @BindView(R.id.text)
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-    }
-
-    private void initView() {
-        tv1 = (TextView) findViewById(R.id.tv1);
-        wb1 = (WebView) findViewById(R.id.wb1);
-        tv1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wb1.loadUrl("http://172.17.8.100/small/commodity/v1/bannerShow");
-                wb1.setWebViewClient(new WebViewClient());
-            }
-        });
+        ButterKnife.bind(this);
     }
 }
